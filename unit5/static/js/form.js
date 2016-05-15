45fdf6b771bb06ce9dd2a68689a20ee4328ed28f
ishmur <mateuszpab@gmail.com>
2016-05-15 14:47:54 +0200

    Smooth Scrolling

--- unit5/static/js/form.js
+++ unit5/static/js/form.js
@@ -1,4 +1,3 @@
-
 var multiCheckbox = document.getElementById("multiCheckbox");
 var multiDivArray = document.getElementsByClassName("div-multi");
 
@@ -10,6 +9,8 @@ var resignDivArray = document.getElementsByClassName("div-resign");
 
 var guildRadioButton = document.getElementById("guildRadio");
 var guildDivArray = document.getElementsByClassName("div-guild");
+
+var anchorTop = "anchorTop"; // anchored to <body> tag
 
 function showSingleDiv(){
 
@@ -74,6 +75,60 @@ function showGuildDiv(){
         }
     }
 }
+
+function ScrollTop(){
+    // Set delay for <a href> accordion links
+    setTimeout(function(){
+        smoothScroll(anchorTop);
+    }, 150);
+}
+
+/////////////////////////////////////////////////////////////////////////////// Smooth Scroll Function Block
+function smoothScroll(eID) {
+    var startY = currentYPosition();
+    var stopY = elmYPosition(eID);
+    var distance = stopY > startY ? stopY - startY : startY - stopY;
+    if (distance < 100) {
+        scrollTo(0, stopY); return;
+    }
+    var speed = Math.round(distance / 100);
+    if (speed >= 20) speed = 20;
+    var step = Math.round(distance / 25);
+    var leapY = stopY > startY ? startY + step : startY - step;
+    var timer = 0;
+    if (stopY > startY) {
+        for ( var i=startY; i<stopY; i+=step ) {
+            setTimeout("window.scrollTo(0, "+leapY+")", timer * speed);
+            leapY += step; if (leapY > stopY) leapY = stopY; timer++;
+        } return;
+    }
+    for ( var i=startY; i>stopY; i-=step ) {
+        setTimeout("window.scrollTo(0, "+leapY+")", timer * speed);
+        leapY -= step; if (leapY < stopY) leapY = stopY; timer++;
+    }
+}
+
+function currentYPosition() {
+    // Firefox, Chrome, Opera, Safari
+    if (self.pageYOffset) return self.pageYOffset;
+    // Internet Explorer 6 - standards mode
+    if (document.documentElement && document.documentElement.scrollTop)
+        return document.documentElement.scrollTop;
+    // Internet Explorer 6, 7 and 8
+    if (document.body.scrollTop) return document.body.scrollTop;
+    return 0;
+}
+
+function elmYPosition(eID) {
+    var elm = document.getElementById(eID);
+    var y = elm.offsetTop;
+    var node = elm;
+    while (node.offsetParent && node.offsetParent != document.body) {
+        node = node.offsetParent;
+        y += node.offsetTop;
+    } return y;
+}
+///////////////////////////////////////////////////////////////////////////////
 
 window.onload = function() {
     showSingleDiv();
