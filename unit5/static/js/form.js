e137f7adfb23117e089551ead3efff2b3f61c0bb
ishmur <mateuszpab@gmail.com>
2016-05-15 16:15:27 +0200

    Form Default Values
    Added default values (input type="hidden") for div containers for multi,
    single, guild and resign questions

--- unit5/static/js/form.js
+++ unit5/static/js/form.js
@@ -1,78 +1,91 @@
-var multiCheckbox = document.getElementById("multiCheckbox");
-var multiDivArray = document.getElementsByClassName("div-multi");
-
 var singleCheckbox = document.getElementById("singleCheckbox");
-var singleDivArray = document.getElementsByClassName("div-single");
+var singleDivClass = "div-single";
+
+var multiCheckbox = document.getElementById("multiCheckbox");
+var multiDivClass = "div-multi";
 
 var resignRadioButton = document.getElementById("resignRadio");
-var resignDivArray = document.getElementsByClassName("div-resign");
+var resignDivClass = "div-resign";
 
 var guildRadioButton = document.getElementById("guildRadio");
-var guildDivArray = document.getElementsByClassName("div-guild");
+var guildDivClass = "div-guild";
 
 var anchorTop = "anchorTop"; // anchored to <body> tag
+
 
 function showSingleDiv(){
 
     var iterator;
+    var singleDivArray = document.getElementsByClassName(singleDivClass);
 
     if (singleCheckbox.checked){
         for (iterator=0; iterator < singleDivArray.length; iterator++){
             singleDivArray[iterator].style.display = "block";
         }
+        uncheckAll(singleDivClass,'show');
     }
     else {
         for (iterator=0; iterator < singleDivArray.length; iterator++){
             singleDivArray[iterator].style.display = "none";
         }
+        uncheckAll(singleDivClass,'hide');
     }
 }
 
 function showMultiDiv(){
 
     var iterator;
+    var multiDivArray = document.getElementsByClassName(multiDivClass);
 
     if (multiCheckbox.checked){
         for (iterator=0; iterator < multiDivArray.length; iterator++){
             multiDivArray[iterator].style.display = "block";
         }
+        uncheckAll(multiDivClass,'show');
     }
     else {
         for (iterator=0; iterator < multiDivArray.length; iterator++){
             multiDivArray[iterator].style.display = "none";
         }
+        uncheckAll(multiDivClass,'hide');
     }
 }
 
 function showResignDiv(){
 
     var iterator;
+    var resignDivArray = document.getElementsByClassName(resignDivClass);
 
     if (resignRadioButton.checked){
         for (iterator=0; iterator < resignDivArray.length; iterator++){
             resignDivArray[iterator].style.display = "block";
         }
+        uncheckAll(resignDivClass,'show');
     }
     else {
         for (iterator=0; iterator < resignDivArray.length; iterator++){
             resignDivArray[iterator].style.display = "none";
         }
+        uncheckAll(resignDivClass,'hide');
     }
 }
 
 function showGuildDiv(){
 
     var iterator;
+    var guildDivArray = document.getElementsByClassName("div-guild");
 
     if (guildRadioButton.checked){
         for (iterator=0; iterator < guildDivArray.length; iterator++){
             guildDivArray[iterator].style.display = "block";
         }
+        uncheckAll(guildDivClass,'show');
     }
     else {
         for (iterator=0; iterator < guildDivArray.length; iterator++){
             guildDivArray[iterator].style.display = "none";
         }
+        uncheckAll(guildDivClass,'hide');
     }
 }
 
@@ -129,6 +142,38 @@ function elmYPosition(eID) {
     } return y;
 }
 ///////////////////////////////////////////////////////////////////////////////
+
+function uncheckAll(divClass, actionString) {
+    var inputsAll = document.querySelectorAll('div.' + divClass + ' input');
+    var inputsHidden = document.querySelectorAll('div.' + divClass + ' input[type="hidden"]');
+    var input;
+    var i;
+
+    switch(actionString){
+        case 'hide':
+            for(i =0; i< inputsAll.length;i++){
+                var input = inputsAll[i];
+                input.checked = false;
+            }
+            checks = document.querySelectorAll('div.' + divClass + ' input[type="hidden"]');
+            for(i =0; i< checks.length;i++){
+                var input = checks[i];
+                input.checked = true;
+            }
+            break;
+
+        case 'show':
+            checks = document.querySelectorAll('div.' + divClass + ' input[type="hidden"]');
+            for(i =0; i< checks.length;i++){
+                var input = checks[i];
+                input.checked = false;
+            }
+            break;
+
+        default:
+            break;
+    }
+}
 
 window.onload = function() {
     showSingleDiv();
